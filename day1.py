dbe8f7e3665bd800b9ce7ce18850aa1d8302594a
Twooey <michael@thewireless.ninja>
2020-12-06 17:13:05 -0500

    Moved to using get_data. This requires a token.input file.

--- day1.py
+++ day1.py
@@ -1,15 +1,17 @@
-with open('day1.txt', 'rb') as f:
-    mylist = set(map(int, f))
-    templist = mylist.copy()
-    stop = False
-    for i in mylist:
-        for j in templist:
-            c = 2020 - i - j
-            if c in mylist:
-                print(i, j, c, i*j*c)
-                stop = True
-                break
-        if stop == True:
+from aocd import get_data
+
+mylist = set(map(int, get_data(open(file='token.input').read(), day=1, year=2020).splitlines()))
+
+templist = mylist.copy()
+stop = False
+for i in mylist:
+    for j in templist:
+        c = 2020 - i - j
+        if c in mylist:
+            print(i, j, c, i*j*c)
+            stop = True
             break
-        else:
-            templist.pop()
+    if stop == True:
+        break
+    else:
+        templist.pop()
